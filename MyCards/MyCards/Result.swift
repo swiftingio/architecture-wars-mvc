ac7c434529ee10a081032bc5317a10a069dc63ac
paciej00 <paciej00@gmail.com>
2017-01-06 18:23:46 +0100

    Remove unused `Result` code

--- MyCards/MyCards/Result.swift
+++ MyCards/MyCards/Result.swift
@@ -14,49 +14,6 @@ enum ResultError: Error {
 enum Result<T> {
     case success(T)
     case failure(Error)
-
-    init(optional value: T?, error: @autoclosure() -> Error) {
-        self = value.map(Result.success) ?? .failure(error())
-    }
-
-    init(throwing value: @autoclosure() throws -> T) {
-        do {
-            self = .success(try value())
-        } catch {
-            self = .failure(error)
-        }
-    }
-
-    var isSuccess: Bool {
-        if case .success(_) = self {
-            return true
-        }
-        return false
-    }
-
-    var optional: T? {
-        if case .success(let value) = self {
-            return value
-        } else {
-            return nil
-        }
-    }
-
-    func forceValue() throws -> T {
-        if case .success(let value) = self {
-            return value
-        } else {
-            throw ResultError.noValue
-        }
-    }
-
-    var error: Error? {
-        if case .failure(let error) = self {
-            return error
-        } else {
-            return nil
-        }
-    }
 }
 
 func ==<T: Equatable> (lhs: Result<T>, rhs: Result<T>) -> Bool {
@@ -69,25 +26,5 @@ func ==<T: Equatable> (lhs: Result<T>, rhs: Result<T>) -> Bool {
 
     default:
         return false
-    }
-}
-
-extension Result {
-    func map<U>(_ transform: (T) -> U) -> Result<U> {
-        switch self {
-        case .success(let value):
-            return .success(transform(value))
-        case .failure(let error):
-            return .failure(error)
-        }
-    }
-
-    func flatMap<U>(_ transform: (T) -> Result<U>) -> Result<U> {
-        switch self {
-        case .success(let value):
-            return transform(value)
-        case .failure(let error):
-            return .failure(error)
-        }
     }
 }
