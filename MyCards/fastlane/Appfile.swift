3c95cae1460e46bc5dc38b069d0da6711f8d2934
paciej00 <2279666+paciej00@users.noreply.github.com>
2018-03-18 14:09:36 +0800

    Adds fastlane integration

---
+++ MyCards/fastlane/Appfile.swift
@@ -0,0 +1,5 @@
+var appIdentifier: String { return "io.swifting.mycards" } // The bundle identifier of your app
+var appleID: String { return "[[APPLE_ID]]" } // Your Apple email address
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
